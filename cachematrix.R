b9c7d3e812a8dc40d39409965dfacfedd6faeec5
Prithu Verma <prithu.pisces@gmail.com>
2018-09-11 00:56:21 +0530

    Submission
    My assignment submission. Added an extra function for testing runtime of the functions involved.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: Creates a matrix object to cache its inverse.
+## cacheSolve: Retrives inverse stored if available, otherwise calls makeCacheMatrix().
+## timetest: Function to check time taken to run the two functions.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
+## Sub-Functions:
+## set()
+## get()
+## setinv()
+## getinv()
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x=matrix())
+{
+	invM<-NULL
+	inverse<-NULL
+	set<-function(y)
+	{
+		x<<-y
+		invM<<-NULL
+	}
+	get<-function() {x}
+	setinv<-function(inverse) {invM<<-inverse}
+	getinv<-function() {inverse}
+	list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix()
+## above. If the inverse has already been calculated (and the matrix has not changed), then
+## the cacheSolve() should retrieve the inverse from the cache.
 
-## Write a short comment describing this function
+cacheSolve <- function(x, ...)
+{
+	invM<-x$getinv()
+	if(!is.null(invM))
+	{
+		message("Processing Data from Cache...")
+		return(invM)
+	}
+	data<-x$get()
+	invM<-solve(data, ...)
+	x$setinv(invM)
+	invM
+}
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function tests the time taken to run makeCacheMatrix() and cacheSolve().
+
+timetest<-function(matrix)
+{
+	temp<-makeCacheMatrix(matrix)
+	start.time<-Sys.time()
+	cacheSolve(temp)
+	duration<-Sys.time()-start.time
+	print(duration)
 }
